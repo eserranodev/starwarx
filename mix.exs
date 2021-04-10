d408564d5500c9fe6e4bf7c657f33e3d842b05ef
Enrique Serrano <eserranodev@gmail.com>
2021-04-10 17:25:18 +0200

    Upgrade Elixir to 1.11.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Starwarx.MixProject do
     [
       app: :starwarx,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
